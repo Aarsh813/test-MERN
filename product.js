d00a6d0f238f6d8d13146f7248fa97da2b54af4b
Aarsh <aarshsonani@gmail.com>
2023-10-23 04:52:21 +0530

    First commit

---
+++ product.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+let dataSchema = mongoose.Schema({
+    
+    'pname': {
+        required: true,
+        type : String
+    },
+    'pprice': {
+        required: true,
+        type : String
+    },
+    'pdetails': {
+        required: true,
+        type : String
+    }
+});
+
+module.exports = mongoose.model("node_js",dataSchema);
---
+++ server.js
@@ -0,0 +1,114 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const mongodb = require("mongodb");
+const product = require("../lib/node_old/product");
+const app = express();
+const cors = require("cors");
+
+const productData = [];
+const Product = require('./product');
+const DB = 'mongodb+srv://Aarsh:Aarsh"12"@cluster0.91ortpx.mongodb.net/?retryWrites=true&w=majority';
+const PORT = 2000;
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(cors());
+
+//post api
+app.post("/api/add_product", async (req, res) => {
+
+    console.log("Result", req.body);
+
+    let data = Product(req.body);
+
+    try{
+        let dataToStore = await data.save();
+        res.status(200).json(dataToStore);
+    }catch(error){
+        res.status(400).json(
+            {'status' : error.message}
+        )
+
+    }
+    // const pdata = {
+    //     "id": productData.length + 1,
+    //     "pname": req.body.pname,
+    //     "pprice": req.body.pprice,
+    //     "pdetails": req.body.pdetails,
+    // };
+
+    // productData.push(pdata);
+
+    // res.status(200).send({
+    //     "status_code": 200,
+    //     "message": "Product added successfully",
+    //     "product": pdata,
+    // })
+})
+
+app.get("/api/get_product",async (req, res) => {
+
+    try {
+        let data = await Product.find();
+        res.status(200).json(data);
+        
+    } catch (error) {
+        res.status(400).json({
+            'status' : error.message
+        })
+    }
+})
+
+//update
+app.patch("/api/update/:id",async (req,res)=>{ //patch method no need to give whole data only the field to change
+
+    let id = req.params.id;
+    let updatedData = req.body;
+    let options = {new: true};
+
+    try {
+        const data = await Product.findByIdAndUpdate(id.updatedData, options);
+        res.status(200).json(data);
+        
+    } catch (error) {
+        res.status(400).json({
+            'status' : error.message
+        })
+    }
+})
+
+//delete
+app.delete("/api/delete/:id",async (req,res)=>{
+
+    let id = req.params.id;
+
+    try {
+        const data = await Product.findByIdAndDelete(id);
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(400).json({
+            'status' : error.message
+        })
+    }
+})
+
+//connect
+mongoose
+    .connect(DB)
+    .then(() => {
+        console.log('Connected to Database Successfully');
+    })
+    .catch((e) => {
+        console.log(e.message);
+    });
+app.listen(PORT, () => {
+    console.log("Connected to port " + PORT);
+})
+
+
+
+
+
